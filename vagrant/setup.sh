6b6ed2265a33b4987415f157a2188e1a6bf643f8
川部勝也 <al14031@shibaura-it.ac.jp>
2017-05-25 18:26:06 +0900

    fix vagrant setup

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -6,5 +6,6 @@ mkdir ./vagrant_centos7
 cd vagrant_centos7
 
 vagrant box add centos-7.1 https://github.com/CommanderK5/packer-centos-template/releases/download/0.7.1/vagrant-centos-7.1.box
+vagrant init
 vagrant up
 vagrant ssh
