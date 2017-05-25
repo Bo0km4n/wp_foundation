7d0b56de817054ca01280d1c446a85eb299252db
川部勝也 <al14031@shibaura-it.ac.jp>
2017-05-25 18:24:12 +0900

    add vagrant setting

---
+++ vagrant/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Start setup Vagrant..."
+
+mkdir ./vagrant_centos7
+cd vagrant_centos7
+
+vagrant box add centos-7.1 https://github.com/CommanderK5/packer-centos-template/releases/download/0.7.1/vagrant-centos-7.1.box
+vagrant up
+vagrant ssh
